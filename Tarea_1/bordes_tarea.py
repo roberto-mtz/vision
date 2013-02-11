c565d18a1054050987e5db2e60fa2d0e630f2f84
Roberto <roberto.martinezmn@gmail.com>
2013-02-10 20:15:37 -0600

    Tarea 1

--- Tarea_1/bordes_tarea.py
+++ Tarea_1/bordes_tarea.py
@@ -1,6 +1,8 @@
 from Tkinter import *
 from PIL import Image, ImageTk
 from math import floor
+import time
+import numpy
 
 def ventana():
     root = Tk()
@@ -9,15 +11,11 @@ def ventana():
     frame.pack(padx=5,pady=5)
     poner_imagen(obtener_original(path_imagen_original))
     b1 = Button(text='Original', command = boton_original).pack(in_=frame, side=LEFT)
-    b2 = Button(text='Grises', command = boton_grises).pack(in_=frame, side=LEFT)
-    b3 = Button(text='Promedio', command = boton_promedio).pack(in_=frame, side=LEFT)
-    b4 = Button(text='Diferencia', command = boton_diferencia).pack(in_=frame, side=LEFT)
-    b5 = Button(text='Gaussian', command = boton_convolucion).pack(in_=frame, side=LEFT)
-    b6 = Button(text='Umbral', command = boton_umbral).pack(in_=frame, side=LEFT)
+    b2 = Button(text='Detectar bordes', command = boton_bordes).pack(in_=frame, side=LEFT)
+    b1 = Button(text='Prueba tiempo', command = boton_prueba).pack(in_=frame, side=LEFT)
     root.mainloop()
 
 def poner_imagen(image):
-    #image = Image.open()
     photo = ImageTk.PhotoImage(image)
     global label
     label = Label(image=photo)
@@ -28,9 +26,9 @@ def cambiar_agrises(path_original):
     imagen = Image.open(path_imagen_original).convert("RGB")
     pixeles = imagen.load()
     x, y = imagen.size
-
+    
     imagen_nueva = Image.new("RGB", (x, y))
-
+    
     colores = []
     for a in range(x):
         for b in range(y):
@@ -39,9 +37,7 @@ def cambiar_agrises(path_original):
             tupla_promedio = (promedio, promedio, promedio)
             colores.append(tupla_promedio)
             imagen_nueva.putpixel((a, b), tupla_promedio)
-
-    #imagen_nueva.save(path_imagen)
-    #print colores
+    
     return imagen_nueva
 
 def convolucion(f, h):
@@ -64,15 +60,14 @@ def convolucion(f, h):
             suma = int(floor(suma))
             tupla_promedio = (suma, suma, suma)
             F.putpixel((a,b),tupla_promedio)
-     
+    
     return F
 
 def cambiar_promedio(imagen):
-    #imagen = Image.open(path_imagen_original).convert("RGB")
     pixeles = imagen.load()
     x, y = imagen.size
     imagen_nueva_prom = Image.new("RGB", (x, y))
-
+    
     colores = []
     for a in range(x):
         for b in range(y):
@@ -80,7 +75,7 @@ def cambiar_promedio(imagen):
             veces = 5
             suma = 0
             promedio = 0
-
+            
             try:
                 pixel_norte = pixeles[a-1,b]
             except IndexError:
@@ -101,7 +96,7 @@ def cambiar_promedio(imagen):
             except IndexError:
                 pixel_oeste = (0, 0, 0)
                 veces = veces - 1
-
+            
             Rojos_suma = pixel_norte[0] + pixel_sur[0] + pixel_este[0] + pixel_oeste[0] + pixel_color[0]
             Verdes_suma = pixel_norte[1]+ pixel_sur[1] + pixel_este[1] + pixel_oeste[1] + pixel_color[1]
             Azul_suma = pixel_norte[2]+ pixel_sur[2] + pixel_este[2] + pixel_oeste[2] + pixel_color[2]
@@ -109,21 +104,18 @@ def cambiar_promedio(imagen):
             Rojo_prom = Rojos_suma/veces
             Verdes_prom = Verdes_suma/veces
             Azul_prom = Azul_suma/veces
-
+            
             tupla_promedio = (Rojo_prom, Verdes_prom, Azul_prom)
             colores.append(tupla_promedio)
             imagen_nueva_prom.putpixel((a, b), tupla_promedio)
-
-#imagen_nueva_prom.save("hola_pruebo.gif")
-
+    
     return imagen_nueva_prom
 
 def diferencia(imagen):
-    #imagen = Image.open(path_imagen_original).convert("RGB")                                                    
     pixeles = imagen.load()
     x, y = imagen.size
     imagen_nueva_prom = Image.new("RGB", (x, y))
-
+    
     colores = []
     for a in range(x):
         for b in range(y):
@@ -131,7 +123,7 @@ def diferencia(imagen):
             veces = 5
             suma = 0
             promedio = 0
-
+            
             try:
                 pixel_norte = pixeles[a-1,b]
             except IndexError:
@@ -168,14 +160,43 @@ def diferencia(imagen):
             tupla_promedio = (Rojo_dif, Verde_dif, Azul_dif)
             colores.append(tupla_promedio)
             imagen_nueva_prom.putpixel((a, b), tupla_promedio)
-
-#imagen_nueva_prom.save("hola_pruebo.gif")
-
+    
     return imagen_nueva_prom
 
+def normalizacion(imagen):
+    pixeles = imagen.load()
+    x, y = imagen.size
+    min = 0
+    max = 0
+    imagen_nueva = Image.new("RGB", (x, y))
+    
+    for a in range(x):
+        for b in range(y):
+            pixel_color = pixeles[a, b]
+            max_ant = max
+            max_new = pixel_color[0]
+            min_ant = min
+            min_new = pixel_color[0]
+            if (max_new >= max_ant):
+                max = max_new
+            elif (min_new <= min_ant):
+                min = min_new
+            elif (min == 0 and max == 255):
+                break
+
+    print min
+    print max
+    for a in range(x):
+        for b in range(y):
+            pixel_color = pixeles[a, b]
+            nuevo_pixel = ( float(pixel_color[0]) - float(min) )*( float(255) / (float(max) - float(min)) )
+            nuevo_pixel = int(floor(nuevo_pixel))
+            tupla_promedio = (nuevo_pixel, nuevo_pixel, nuevo_pixel)
+            imagen_nueva.putpixel((a, b), tupla_promedio)
+
+    return imagen_nueva
 
 def cambiar_umbral(imagen, umbral_valor):
-    #imagen = Image.open(path_imagen_original).convert("RGB")
     pixeles = imagen.load()
     x, y = imagen.size
     imagen_nueva = Image.new("RGB", (x, y))
@@ -191,9 +212,6 @@ def cambiar_umbral(imagen, umbral_valor):
                 poner_pixel = 0
             tupla_pixel = (poner_pixel, poner_pixel, poner_pixel)
             imagen_nueva.putpixel((a, b), tupla_pixel)
-    
-    #imagen_nueva.save(path_imagen)
-    #print colores
     return imagen_nueva
 
 
@@ -201,44 +219,51 @@ def obtener_original(path_imagen_original):
     imagen = Image.open(path_imagen_original)
     return imagen
 
-def boton_grises():
+def boton_bordes():
+    inicio = time.time()
     label.destroy()
     imagen_grises = cambiar_agrises(path_imagen_original)
-    poner_imagen(imagen_grises)
+    imagen_grises.save("paso_1.jpg")
+    imagen_prom1 = cambiar_promedio(imagen_grises.convert("RGB"))
+    imagen_prom1.save("paso_2.jpg")
+    imagen_prom2 = cambiar_promedio(imagen_prom1.convert("RGB"))
+    imagen_prom2.save("paso_3.jpg")
+    imagen_prom = diferencia(imagen_prom2.convert("RGB"))
+    imagen_prom.save("paso_4.jpg")
+    h = numpy.array([[-3,-3,5],[-3,0,5],[-3,-3,5]])
+    imagen_con = convolucion(imagen_prom, h)
+    imagen_con.save("paso_5.jpg")
+    h_hori = numpy.array([[-1, -2, -1],[0, 0, 0], [1, 2, 1]])
+    imagen_con2 = convolucion(imagen_con, h_hori)
+    imagen_con3 = convolucion(imagen_con2, h_hori)
+    imagen_con3.save("paso_6.jpg")
+    h_verti = numpy.array([[-1, 0, 1],[-2, 0, 2],[-1, 0, 1]])
+    imagen_con4 = convolucion(imagen_con3, h_verti)
+    imagen_con4.save("paso_7.jpg")
+    imagen_nor = normalizacion(imagen_con4)
+    imagen_nor.save("paso_8.jpg")
+    umbral_valor = 0.5
+    imagen_bin = cambiar_umbral(imagen_nor.convert("RGB"), umbral_valor)
+    imagen_bin.save("paso_9.jpg")
+    imagen_final1 = cambiar_promedio(imagen_bin.convert("RGB"))
+    imagen_final1.save("paso_10.jpg")
+    poner_imagen(imagen_final1)
+    fin = time.time()
+    tiempo = fin - inicio
+    print "Tiempo que trascurrio -> " + str(tiempo)
+    return tiempo
 
 def boton_original():
     label.destroy()
     imagen_original = obtener_original(path_imagen_original)
     poner_imagen(imagen_original)
 
-def boton_promedio():
-    label.destroy()
-    imagen_grises = cambiar_agrises(path_imagen_original)
-    imagen_prom1 = cambiar_promedio(imagen_grises.convert("RGB"))
-    poner_imagen(imagen_prom1)
+def boton_prueba():
+    tiempo = 0.0
+    for i in range(30):
+        tiempo = tiempo + boton_bordes()
+    promedio = tiempo / 30.0
+    print "Tiempo promedio de " + path_imagen_original + " es = " + str(promedio)
 
-def boton_diferencia():
-    label.destroy()
-    imagen_grises = cambiar_agrises(path_imagen_original)
-    #imagen_grises = obtener_original(path_imagen_original)
-    imagen_prom = diferencia(imagen_grises.convert("RGB"))
-    poner_imagen(imagen_prom)
-
-def boton_convolucion():
-    label.destroy()
-    imagen_grises = cambiar_agrises(path_imagen_original)
-    #h = [[0, 0.2, 0],[0.2, 0.2, 0.2],[0,0.2,0]]
-    h = [[(1.0/16.0)*1.0, (1.0/16.0)*2.0, (1.0/16.0)*1.0],[(1.0/16.0)*2.0, (1.0/16.0)*4.0, (1.0/16.0)*2.0],[(1.0/16.0)*1.0, (1.0/16.0)*2.0, (1.0/16.0)*1.0]]
-    imagen_con = convolucion(imagen_grises, h)
-    poner_imagen(imagen_con)
-
-def boton_umbral():
-    label.destroy()
-    umbral_valor = 0.5
-    imagen_grises = cambiar_agrises(path_imagen_original)
-    imagen_umb = cambiar_umbral(imagen_grises.convert("RGB"), umbral_valor)
-    poner_imagen(imagen_umb)
-    
-
-path_imagen_original = "paris.gif"
+path_imagen_original = "cerro.jpg"
 ventana()
